626088bdc47a2392999bcda8075a1ae5568cb191
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-26 21:40:19 +0530

    login and home form Connect

--- src/main/java/contraller/LoginContraller.java
+++ src/main/java/contraller/LoginContraller.java
@@ -1,13 +1,19 @@
 package contraller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginContraller {
     public TextField txtUserName;
     public TextField txtPassword;
 
+    Stage stage = new Stage();
     public void btnLoginAction(ActionEvent e) {
         String userName = "pasindu";
         String password = "12345";
@@ -36,6 +42,12 @@ public class LoginContraller {
             alert.showAndWait();
             return;
         }
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/template/home_form.fxml"))));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        stage.show();
 
 
     }
