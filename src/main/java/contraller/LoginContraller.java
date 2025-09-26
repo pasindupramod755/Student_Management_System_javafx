e44c1ddd0160bf6112e4147c9e087f4de9f3de34
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-26 19:40:13 +0530

    login Action

--- src/main/java/contraller/LoginContraller.java
+++ src/main/java/contraller/LoginContraller.java
@@ -1,4 +1,13 @@
 package contraller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginContraller {
+    public TextField txtUserName;
+    public TextField txtPassword;
+
+    public void btnLoginAction(ActionEvent e) {
+        
+    }
 }
