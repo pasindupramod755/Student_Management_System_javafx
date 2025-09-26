9debbe831f6250663f793827ac0ba29eb799ad2d
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-26 19:51:28 +0530

    login validate

--- src/main/java/contraller/LoginContraller.java
+++ src/main/java/contraller/LoginContraller.java
@@ -1,6 +1,7 @@
 package contraller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class LoginContraller {
@@ -8,6 +9,34 @@ public class LoginContraller {
     public TextField txtPassword;
 
     public void btnLoginAction(ActionEvent e) {
-        
+        String userName = "pasindu";
+        String password = "12345";
+
+        if (txtUserName.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Login Warning");
+            alert.setHeaderText(null);
+            alert.setContentText(" ⚠ Please enter your Username!");
+            alert.showAndWait();
+            return;
+        }
+        if (txtPassword.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Login Warning");
+            alert.setHeaderText(null);
+            alert.setContentText(" ⚠ Please enter your Password!");
+            alert.showAndWait();
+            return;
+        }
+        if (!userName.equals(txtUserName.getText()) || !password.equals(txtPassword.getText())){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Login Warning");
+            alert.setHeaderText(null);
+            alert.setContentText(" ⚠ Username or Password incorrect!");
+            alert.showAndWait();
+            return;
+        }
+
+
     }
 }
