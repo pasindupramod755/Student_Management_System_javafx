4fb82d3d00e35d3dee74aad8fd9222630f81a67c
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-30 21:26:44 +0530

    connet Add Batch

--- src/main/java/contraller/BatchManagementContraller.java
+++ src/main/java/contraller/BatchManagementContraller.java
@@ -1,4 +1,20 @@
 package contraller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class BatchManagementContraller {
+    Stage stage = new Stage();
+    public void btnAddBatchAction(ActionEvent actionEvent) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/template/add_batch.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
 }
