54c883cbefc794962a2b014523fd3e520fd0cb72
pasindupramod755 <pasidubandara12345@gmail.com>
2025-10-01 20:27:23 +0530

    Add PRF Mark UI

---
+++ src/main/java/contraller/GradeManagementContraller.java
@@ -0,0 +1,13 @@
+package contraller;
+
+import javafx.event.ActionEvent;
+
+public class GradeManagementContraller {
+    public void btnPRFMarkUpdateAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDBMSMarkUpdateAction(ActionEvent actionEvent) {
+
+    }
+}
