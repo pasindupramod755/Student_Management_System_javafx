857c1c027e3d6de62d383e467c4ecbbf1f827ece
pasindupramod755 <pasidubandara12345@gmail.com>
2025-10-04 19:03:01 +0530

    connect  Student Registration Report

--- src/main/java/contraller/ReportManagementContraller.java
+++ src/main/java/contraller/ReportManagementContraller.java
@@ -1,4 +1,20 @@
 package contraller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ReportManagementContraller {
+    Stage stage = new Stage();
+    public void btnStudentRegistrationReportAction(ActionEvent actionEvent) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/template/student_registration_report.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
 }
